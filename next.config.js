7fedea695ad2b9b182fe15552736ec82cf391310
martinc2907 <martinc2907@krafton.com>
2021-12-13 13:16:13 +0900

    add server domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    ROOT: process.env.NODE_ENV === "production" ? "" : "http://localhost:3000",
+    ROOT:
+      process.env.NODE_ENV === "production"
+        ? "https://nextjs-minimal-todo.vercel.app/"
+        : "http://localhost:3000",
   },
 }
